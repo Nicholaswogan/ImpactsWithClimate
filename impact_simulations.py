3b6a58b2cf44b72c83799f49735c905675bf0716
Nick Wogan <nicholaswogan@gmail.com>
2022-08-31 13:22:33 +0200

    added option to do a perfect CO2 conversion

--- impact_simulations.py
+++ impact_simulations.py
@@ -11,7 +11,7 @@ class Constants:
     yr = 365*24*60*60
 cons = Constants()
 
-def impact_evolve(init, settings_in, outfile, eddy, RH, P_top, T_trop, T_guess, zero_out, nz, rainfall_rate, P_top_min, atol, rtol, t_eval, restart_from_file, T_surf_guess):
+def impact_evolve(init, settings_in, outfile, eddy, RH, P_top, T_trop, T_guess, zero_out, nz, rainfall_rate, P_top_min, atol, rtol, t_eval, restart_from_file, T_surf_guess, perfect_conversion):
     settings_out = outfile+"_settings.yaml"
     atmosphere_out = outfile+"_atmosphere.txt"
     
@@ -22,6 +22,13 @@ def impact_evolve(init, settings_in, outfile, eddy, RH, P_top, T_trop, T_guess, 
         M_i = init['M_i']
         stm = SteamAtm('zahnle_earth_ct.yaml')
         sol_stm = stm.impact(N_H2O_ocean,N_CO2,N_N2,M_i)
+        if perfect_conversion:
+            # We convert all CO2 to CH4 using H2 in the atmosphere.
+            CO2 = sol_stm['CO2'][-1]
+            sol_stm['CO2'][-1] = sol_stm['CO2'][-1] - CO2
+            sol_stm['H2'][-1] = sol_stm['H2'][-1] - 4*CO2
+            sol_stm['CH4'][-1] = sol_stm['CH4'][-1] + CO2
+            sol_stm['H2O'][-1] = sol_stm['H2O'][-1] + 2*CO2
 
         c = WaterAdiabatClimate('input/adiabat_species.yaml', \
                                 'input/adiabat_settings.yaml', \
@@ -36,7 +43,7 @@ def impact_evolve(init, settings_in, outfile, eddy, RH, P_top, T_trop, T_guess, 
                        atmosphere_out)
 
     pc.var.mxsteps = 100000
-    pc.var.max_error_reinit_attempts = 5
+    pc.var.max_error_reinit_attempts = 100
     pc.T_trop = T_trop
     pc.P_top_min = P_top_min
     pc.P_top_max = 1e10
@@ -58,6 +65,7 @@ def nominal(restart_from_file=False, T_surf_guess=300):
     init['N_N2'] = 36.
     init['M_i'] = 2.0e24
     params['init'] = init
+    params['perfect_conversion'] = False
 
     params['settings_in'] = "input/settings_Hadean.yaml"
     params['outfile'] = "results/nominal"
@@ -88,9 +96,134 @@ def pretty_big(restart_from_file=False, T_surf_guess=300):
     init['N_N2'] = 36.
     init['M_i'] = 1.5e24
     params['init'] = init
+    params['perfect_conversion'] = False
 
     params['settings_in'] = "input/settings_Hadean.yaml"
     params['outfile'] = "results/pretty_big"
+    params['eddy'] = 1e6
+    params['RH'] = 1.0
+    params['P_top'] = 1.0e-1
+    params['T_trop'] = 200
+    params['T_guess'] = 400
+    params['zero_out'] = ['NH3']
+    params['nz'] = 100
+    params['rainfall_rate'] = 1
+
+    params['P_top_min'] = 1.0e-7
+    params['atol'] = 1e-25
+    params['rtol'] = 1e-3
+    params['t_eval'] = np.logspace(np.log10(cons.yr),np.log10(cons.yr*30e6),1000)
+    params['restart_from_file'] = restart_from_file
+    params['T_surf_guess'] = T_surf_guess
+
+    return params
+
+def pretty_big_no_rain(restart_from_file=False, T_surf_guess=300):
+    params = {}
+
+    init = {}
+    init['N_H2O_ocean'] = 15.0e3
+    init['N_CO2'] = 23.*0.5
+    init['N_N2'] = 36.
+    init['M_i'] = 1.5e24
+    params['init'] = init
+    params['perfect_conversion'] = False
+
+    params['settings_in'] = "input/settings_Hadean.yaml"
+    params['outfile'] = "results/pretty_big_no_rain"
+    params['eddy'] = 1e6
+    params['RH'] = 1.0
+    params['P_top'] = 1.0e-1
+    params['T_trop'] = 200
+    params['T_guess'] = 400
+    params['zero_out'] = ['NH3']
+    params['nz'] = 100
+    params['rainfall_rate'] = -1
+
+    params['P_top_min'] = 1.0e-7
+    params['atol'] = 1e-26
+    params['rtol'] = 1e-3
+    params['t_eval'] = np.logspace(np.log10(cons.yr),np.log10(cons.yr*30e6),1000)
+    params['restart_from_file'] = restart_from_file
+    params['T_surf_guess'] = T_surf_guess
+
+    return params
+
+def pretty_big_lower_vdep(restart_from_file=False, T_surf_guess=300):
+    params = {}
+
+    init = {}
+    init['N_H2O_ocean'] = 15.0e3
+    init['N_CO2'] = 23.*0.5
+    init['N_N2'] = 36.
+    init['M_i'] = 1.5e24
+    params['init'] = init
+    params['perfect_conversion'] = False
+
+    params['settings_in'] = "input/settings_Hadean_lowvdep.yaml"
+    params['outfile'] = "results/pretty_big_lower_vdep"
+    params['eddy'] = 1e6
+    params['RH'] = 1.0
+    params['P_top'] = 1.0e-1
+    params['T_trop'] = 200
+    params['T_guess'] = 400
+    params['zero_out'] = ['NH3']
+    params['nz'] = 100
+    params['rainfall_rate'] = 1
+
+    params['P_top_min'] = 1.0e-7
+    params['atol'] = 1e-25
+    params['rtol'] = 1e-3
+    params['t_eval'] = np.logspace(np.log10(cons.yr),np.log10(cons.yr*30e6),1000)
+    params['restart_from_file'] = restart_from_file
+    params['T_surf_guess'] = T_surf_guess
+
+    return params
+
+def pretty_big_warm_strat(restart_from_file=False, T_surf_guess=300):
+    params = {}
+
+    init = {}
+    init['N_H2O_ocean'] = 15.0e3
+    init['N_CO2'] = 23.*0.5
+    init['N_N2'] = 36.
+    init['M_i'] = 1.5e24
+    params['init'] = init
+    params['perfect_conversion'] = False
+
+    params['settings_in'] = "input/settings_Hadean.yaml"
+    params['outfile'] = "results/pretty_big_warm_strat"
+    params['eddy'] = 1e6
+    params['RH'] = 1.0
+    params['P_top'] = 1.0e-1
+    params['T_trop'] = 230
+    params['T_guess'] = 400
+    params['zero_out'] = ['NH3']
+    params['nz'] = 100
+    params['rainfall_rate'] = 1
+
+    params['P_top_min'] = 1.0e-7
+    params['atol'] = 1e-25
+    params['rtol'] = 1e-3
+    params['t_eval'] = np.logspace(np.log10(cons.yr),np.log10(cons.yr*30e6),1000)
+    params['restart_from_file'] = restart_from_file
+    params['T_surf_guess'] = T_surf_guess
+
+    return params
+
+def sorta_pretty_big(restart_from_file=False, T_surf_guess=300):
+    params = {}
+
+    init = {}
+    init['N_H2O_ocean'] = 15.0e3
+    init['N_CO2'] = 23.*0.5
+    init['N_N2'] = 36.
+    init['M_i'] = 1.2e24
+    params['init'] = init
+    params['perfect_conversion'] = False
+
+    params['settings_in'] = "input/settings_Hadean.yaml"
+    params['outfile'] = "results/sorta_pretty_big"
     params['eddy'] = 1e6
     params['RH'] = 1.0
     params['P_top'] = 1.0e-1
@@ -118,6 +251,7 @@ def sorta_big(restart_from_file=False, T_surf_guess=300):
     init['N_N2'] = 36.
     init['M_i'] = 1.0e24
     params['init'] = init
+    params['perfect_conversion'] = False
 
     params['settings_in'] = "input/settings_Hadean.yaml"
     params['outfile'] = "results/sorta_big"
@@ -148,6 +282,7 @@ def less_big(restart_from_file=False, T_surf_guess=300):
     init['N_N2'] = 36.
     init['M_i'] = 6.0e23
     params['init'] = init
+    params['perfect_conversion'] = False
 
     params['settings_in'] = "input/settings_Hadean.yaml"
     params['outfile'] = "results/less_big"
@@ -178,6 +313,7 @@ def vesta(restart_from_file=False, T_surf_guess=300):
     init['N_N2'] = 36.
     init['M_i'] = 2.589e23
     params['init'] = init
+    params['perfect_conversion'] = False
 
     params['settings_in'] = "input/settings_Hadean.yaml"
     params['outfile'] = "results/vesta"
@@ -194,6 +330,37 @@ def vesta(restart_from_file=False, T_surf_guess=300):
     params['atol'] = 1e-25
     params['rtol'] = 1e-3
     params['t_eval'] = np.logspace(np.log10(cons.yr),np.log10(cons.yr*30e6),1000)
+    params['restart_from_file'] = restart_from_file
+    params['T_surf_guess'] = T_surf_guess
+
+    return params
+
+def perfect_vesta(restart_from_file=False, T_surf_guess=300):
+    params = {}
+
+    init = {}
+    init['N_H2O_ocean'] = 15.0e3
+    init['N_CO2'] = 23.*0.5
+    init['N_N2'] = 36.
+    init['M_i'] = 2.589e23
+    params['init'] = init
+    params['perfect_conversion'] = True
+
+    params['settings_in'] = "input/settings_Hadean.yaml"
+    params['outfile'] = "results/perfect_vesta"
+    params['eddy'] = 1e6
+    params['RH'] = 1.0
+    params['P_top'] = 1.0e-1
+    params['T_trop'] = 200
+    params['T_guess'] = 400
+    params['zero_out'] = ['NH3']
+    params['nz'] = 100
+    params['rainfall_rate'] = 1
+
+    params['P_top_min'] = 1.0e-7
+    params['atol'] = 1e-25
+    params['rtol'] = 1e-3
+    params['t_eval'] = np.logspace(np.log10(cons.yr),np.log10(cons.yr*10e6),1000)
     params['restart_from_file'] = restart_from_file
     params['T_surf_guess'] = T_surf_guess
 
--- movie.py
+++ movie.py
@@ -15,47 +15,56 @@ def make_movie(params, location, istart):
                 "input/Sun_4.0Ga.txt",\
                 params['outfile']+"_atmosphere.txt")
 
-    species = ['H2','N2','CO2','H2O','CH4','CO','HCaer1','HCN','HCCCN']
+    # species = ['H2','N2','CO2','H2O','CH4','CO','HCN','HCaer1']
+    # labels = ['H$_2$','N$_2$','CO$_2$','H$_2$O','CH$_4$','CO','HCN','Hydrocarbon\nAerosols']
+    # ls = ['-','-','-','-','-','-','-','--']
+
+    species = ['H2','N2','CO2','H2O','CH4','HCN','CO','HCaer1']
+    labels = ['H$_2$','N$_2$','CO$_2$','H$_2$O','CH$_4$','HCN','CO','Hydrocarbon\nAerosols']
+    ls = ['-','-','-','-','-','--','-',':']
+    colors = ['C3','C5','C2','C0','k','darkblue','C4','k']
+    lw = [2.5,2.5,2.5,2.5,2.5,2.5,2.5,3.0]
 
     for i in range(istart,len(sol_pc['time'])):
         plt.rcParams.update({'font.size': 15})
-        fig,ax = plt.subplots(1,1,figsize=[7,5])
+        fig,ax = plt.subplots(1,1,figsize=[8,5])
         fig.patch.set_facecolor("w")
         
         den = np.sum(sol1['usol'][:,:,i],axis=0)
         pc.regrid_prep_atmosphere(sol1['usol'][:,:,i],sol1['top_atmos'][i])
-        for sp in species:
-            ax.plot(sol_pc[sp][:,i]/den,sol1['alt'][:,i],label=sp)
+        for j,sp in enumerate(species):
+            ax.plot(sol_pc[sp][:,i]/den,sol1['alt'][:,i],label=labels[j],lw=lw[j],ls=ls[j],c=colors[j])
 
-        ax.grid()
-        ax.legend(ncol=2,bbox_to_anchor=(1,1.0),loc='upper left')
+        ax.grid(alpha=.5)
+        ax.legend(ncol=1,bbox_to_anchor=(1,1.0),loc='upper left')
         ax.set_xscale('log')
-        ax.set_xlim(1e-15,2)
+        ax.set_xlim(1e-13,2)
 
         ax.set_ylim(0,sol_pc['alt'][-1,0])
-        message = 'Time = '+'%.5e'%(sol_pc['time'][i]/cons.yr)+' yr' \
-          +'\nSurface Pressure = '+'%.5f'%pc.var.surface_pressure+' bar' \
-          +'\nSurface Temperature = '+'%.2f'%pc.T_surf+' K'
+        message = '\nTime = '+'%.2e'%int(1000+sol_pc['time'][i]/cons.yr)+' years' \
+          +'\nSurface Pressure = '+'%.1f'%pc.var.surface_pressure+' bar' \
+          +'\nSurface Temperature = '+'%.1f'%(pc.T_surf)+' K'
         ax.text(0.02, 1.04, message, \
             size = 15,ha='left', va='bottom',transform=ax.transAxes)
         ax.set_ylabel('Altitude (km)')
-        ax.set_xlabel('Mixing Ratio')
+        ax.set_xlabel('Mixing Ratio\n')
 
         plt.savefig(location+'image-'+"{:04d}".format(i)+'.png',dpi=150,bbox_inches='tight')
         plt.close()
         fmt = '{:10}'
         print(fmt.format(i),fmt.format(len(sol_pc['time'])),end='\r')
 
+
 def make_actual_movie(location):
     cmd = 'rm '+location+'output.mp4'
     subprocess.call(cmd,shell=True)
-    cmd = 'ffmpeg -framerate 16 -i '+location+'image-%04d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" '+location+'output.mp4'
+    cmd = 'ffmpeg -framerate 10 -i '+location+'image-%04d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" '+location+'output.mp4'
     subprocess.call(cmd,shell=True)
 
 if __name__ == "__main__":
     threadpool_limits(limits=4)
 
-    location = '../movie2/'
+    location = '../movie5/'
     istart = 0
 
     make_movie(imp.pretty_big(), location, istart)
