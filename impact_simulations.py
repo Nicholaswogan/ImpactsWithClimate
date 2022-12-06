64cc548f5c05d44c822f3b8fcefa07b99068a297
Nick Wogan <nicholaswogan@gmail.com>
2022-12-06 11:15:06 -0800

    re doing the full simulation

--- impact_simulations.py
+++ impact_simulations.py
@@ -3,19 +3,19 @@ from threadpoolctl import threadpool_limits
 
 from coupling import impact_evolve, cons
 
-def pretty_big(restart_from_file=False, T_surf_guess=300):
+def nominal_full(restart_from_file=False, T_surf_guess=300):
     params = {}
 
     init = {}
     init['N_H2O_ocean'] = 15.0e3
     init['N_CO2'] = 23.*0.5
     init['N_N2'] = 36.
-    init['M_i'] = 1.5e24
+    init['M_i'] = 10.0**24.2
     params['init'] = init
     params['perfect_conversion'] = False
 
     params['settings_in'] = "input/settings_Hadean.yaml"
-    params['outfile'] = "results/evolution/pretty_big"
+    params['outfile'] = "results/evolution/nominal_full"
     params['eddy'] = 1e6
     params['RH'] = 1.0
     params['P_top'] = 1.0e-1
@@ -28,7 +28,7 @@ def pretty_big(restart_from_file=False, T_surf_guess=300):
     params['P_top_min'] = 1.0e-7
     params['atol'] = 1e-25
     params['rtol'] = 1e-3
-    params['t_eval'] = np.logspace(np.log10(cons.yr),np.log10(cons.yr*30e6),1000)
+    params['t_eval'] = np.logspace(np.log10(cons.yr),np.log10(cons.yr*30e6),2000)
     params['restart_from_file'] = restart_from_file
     params['T_surf_guess'] = T_surf_guess
 
@@ -42,7 +42,7 @@ def pretty_big(restart_from_file=False, T_surf_guess=300):
 
 if __name__ == "__main__":
     threadpool_limits(limits=4)
-    impact_evolve(**pretty_big(restart_from_file=True, T_surf_guess=380))
+    impact_evolve(**nominal_full())
     
     
     
